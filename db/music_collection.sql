29ca1f52b934a6b43d3c36f6343aefa05df1a49d
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-15 13:39:24 +0100

    Add database

---
+++ db/music_collection.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE albums(
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255),
+  genre VARCHAR(255),
+  artist_id INT8  REFERENCES artists(id)
+);
+-- 
+-- INSERT INTO artists (name)
+--   VALUES ('blalbla');
+--
+-- INSERT INTO albums (title, genre, artist_id)
+--   VALUES ('The Blalbum', 'weird', 1);
